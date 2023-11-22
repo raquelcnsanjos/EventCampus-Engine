9277bc7b6671824617baddb2841d5edba228b117
Deriki Pereira <derikipereira@gmail.com>
2023-11-22 10:50:22 -0300

    feat: :sparkles: registry service starter scaffold

---
+++ registry-service/src/main/java/br/ufrn/imd/controller/RegistryController.java
@@ -0,0 +1,12 @@
+package br.ufrn.imd.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("registry-service")
+public class RegistryController {
+	/**
+	 * TODO here the microservice end points communication will be implemented
+	 */
+}
