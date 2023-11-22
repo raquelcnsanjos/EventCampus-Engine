655b956a3e38c0732f8b099ec3d5c6d846c93b72
raquelcnsanjos <149261396+raquelcnsanjos@users.noreply.github.com>
2023-11-22 18:23:01 -0300

    feat(event): beginning of Event class implementation
    Properties (eventId, eventName, startTime, endTime) and corresponding getters/setters added to the Event class. Constructor introduced to initialize these properties upon object creation. Also, included a field 'participantIds' to manage a list of participant IDs for the event.

--- event-service/src/main/java/br/ufrn/imd/model/Event.java
+++ event-service/src/main/java/br/ufrn/imd/model/Event.java
@@ -1,8 +1,62 @@
 package br.ufrn.imd.model;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public class Event {
-	/**
-	 * TODO here will be the model of the event object that we will work on in the microservice
-	 * -create attributes, constructors
-	 */
+    private String eventId;
+    private String eventName;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    private List<String> participantIds;
+
+    public Event(String eventId, String eventName, LocalDateTime startTime, LocalDateTime endTime) {
+        this.eventId = eventId;
+        this.eventName = eventName;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    public String getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(String eventId) {
+        this.eventId = eventId;
+    }
+
+    public String getEventName() {
+        return eventName;
+    }
+
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
+    }
+
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalDateTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalDateTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalDateTime endTime) {
+        this.endTime = endTime;
+    }
+
+    public List<String> getParticipantIds() {
+        return participantIds;
+    }
+
+    public void setParticipantIds(List<String> participantIds) {
+        this.participantIds = participantIds;
+    }
+
+    // add other methods
 }
+
